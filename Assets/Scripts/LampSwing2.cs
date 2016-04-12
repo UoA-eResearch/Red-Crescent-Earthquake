05a3b9d75f6e40b1e9fa43bae2ad9191c198ac5f
SamiHamid <samihamid@hotmail.co.uk>
2016-04-12 12:35:48 +0300

    Clean up of Intro Scene + Bug fix on particle effect

---
+++ Assets/Scripts/LampSwing2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LampSwing2 : MonoBehaviour {
+
+	private Animator anim;
+
+
+	void Start () 
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+
+	public void Lamp2Go()
+	{
+		anim.SetTrigger("Go");
+	}
+}
+
